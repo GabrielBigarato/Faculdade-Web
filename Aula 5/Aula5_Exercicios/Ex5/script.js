a2a637f0a2cb357a0759de70e3ada03f52f288d4
Bigarato12 <g.biga1234@gmail.com>
2023-10-20 01:32:29 -0300

    adicionando a prova

--- Aula 5/Aula5_Exercicios/Ex5/script.js
+++ Aula 5/Aula5_Exercicios/Ex5/script.js
@@ -1,26 +1,39 @@
+// Função para calcular o valor a ser pago com base na modalidade selecionada
 function calcularValorAPagar() {
+    // Obtenha o valor do produto do campo de entrada
     var valorProduto = parseFloat(document.getElementById("valorProduto").value);
+    
+    // Obtenha a modalidade de pagamento selecionada
     var modalidade = parseInt(document.getElementById("modalidade").value);
+    
+    // Elemento onde exibiremos o resultado
     var resultadoElement = document.getElementById("resultado");
 
     var valorASerPago;
 
+    // Use uma estrutura de seleção switch para determinar a ação com base na modalidade
     switch (modalidade) {
         case 1:
-            valorASerPago = valorProduto - (0.1 * valorProduto); // 10% de desconto
+            // Se a modalidade for 1 (A VISTA), aplique um desconto de 10%
+            valorASerPago = valorProduto - (0.1 * valorProduto);
             break;
         case 2:
-            valorASerPago = valorProduto + (0.02 * valorProduto); // 2% de juros
+            // Se a modalidade for 2 (DEBITO), aplique uma taxa de juros de 2%
+            valorASerPago = valorProduto + (0.02 * valorProduto);
             break;
         case 3:
-            valorASerPago = valorProduto + (0.07 * valorProduto); // 7% de juros
+            // Se a modalidade for 3 (CREDITO), aplique uma taxa de juros de 7%
+            valorASerPago = valorProduto + (0.07 * valorProduto);
             break;
         default:
+            // Se a modalidade não for 1, 2 ou 3, exiba uma mensagem de erro
             resultadoElement.textContent = "Modalidade de pagamento inválida.";
             return;
     }
 
+    // Exiba o valor do produto e o valor a ser pago no elemento de resultado
     resultadoElement.textContent = "Valor do Produto: R$" + valorProduto.toFixed(2) + "\nValor a Ser Pago: R$" + valorASerPago.toFixed(2);
 
+    // Exiba um alerta com o mesmo resultado
     alert("Valor do Produto: R$" + valorProduto.toFixed(2) + "\nValor a Ser Pago: R$" + valorASerPago.toFixed(2));
 }
--- PROVA/AvParcial/script.js
+++ PROVA/AvParcial/script.js
@@ -5,75 +5,84 @@
 //  GABRIEL BIGARATO - RGM: 32831048
 //  MARIA EDUARDA GOMES LOPES - RGM: 33292345
 // --------------------------------
-
-const dificuldade = document.getElementsByName("dificuldade")
-const btnCriarJogo = document.getElementById("btnCriarJogo")
-var maxTentativas = 0 ;
-
+// Variáveis globais
+const dificuldade = document.getElementsByName("dificuldade");
+const btnCriarJogo = document.getElementById("btnCriarJogo");
+var maxTentativas = 0;
 var tentativas = 0;
-
 var numerosDigitados = [];
+var numeroSecreto;
 
-btnCriarJogo.addEventListener("click", function ajustaDificuldade(){
+const entrada = document.getElementById("entrada");
+const btnEnviarPalpite = document.getElementById("btnEnviarPalpite");
+const saida = document.getElementById("saida");
+const palpites = document.getElementById("palpites");
+
+// Evento de clique no botão "Criar Jogo"
+btnCriarJogo.addEventListener("click", function ajustaDificuldade() {
     if (dificuldade[0].checked == true) {
         maxTentativas = 10;
-        
-    }   else if ( dificuldade[1].checked == true) {
+    } else if (dificuldade[1].checked == true) {
         maxTentativas = 7;
-        
-    }   else if ( dificuldade[2].checked == true) {
+    } else if (dificuldade[2].checked == true) {
         maxTentativas = 5;
-        
-    }   
+    }
 
     if (maxTentativas > 0) {
         novoJogo();
-        
-    }   else{
-        alert("Selecione uma Dificuldade")
+    } else {
+        alert("Selecione uma Dificuldade");
     }
+});
 
-
-})
 function novoJogo() {
+    // Reinicie o contador de tentativas e a lista de números digitados
+    tentativas = 0;
+    numerosDigitados = [];
 
-    numero = Math.random()*100;
-    var numeroSecreto = Math.floor(numero);
+    // Gere um novo número secreto
+    numeroSecreto = Math.floor(Math.random() * 100) + 1;
 
+    // Limpe o valor da entrada de texto e reabilite o botão
+    entrada.value = "";
+    btnEnviarPalpite.disabled = false;
 
-    const entrada = document.getElementById("entrada");
-    const btnEnviarPalpite = document.getElementById("btnEnviarPalpite");
-    const saida = document.getElementById("saida");
-    const palpites = document.getElementById("palpites");
+    // Limpe o conteúdo do elemento "saida" e "palpites"
+    saida.innerHTML = "";
+    palpites.innerHTML = maxTentativas;
 
-    btnEnviarPalpite.addEventListener("click", function avaliaPalpite() {
-
-        if (entrada.value > numeroSecreto) {
-            //alert('O numero de entrada é maior')
-        
-            maxTentativas = maxTentativas - 1
-        
-            saida.innerHTML = "O numero de entrada é maior"
-            
-            
-        
-        } else if (entrada.value < numeroSecreto){
-            //alert('O numero de entrada é menor')
-        
-            maxTentativas = maxTentativas - 1
-        
-            saida.innerHTML = "O numero de entrada é menor"
-        
-            
-        
-        } else {
-            //alert('O numero é igual PARABENS, voce acertou depois de ' + erro + ' erros')
-            
-            saida.innerHTML = "O numero é igual PARABENS"
-        
-        }
-
-        palpites.innerHTML = maxTentativas
-    })
+    // Remova o evento de clique do botão "Enviar Palpite" antes de anexá-lo novamente
+    btnEnviarPalpite.removeEventListener("click", avaliaPalpite);
+    btnEnviarPalpite.addEventListener("click", avaliaPalpite);
 }
 
+// Função para avaliar o palpite
+function avaliaPalpite() {
+    const palpite = parseInt(entrada.value);
+
+    if (isNaN(palpite)) {
+        saida.innerHTML = "Por favor, insira um número válido.";
+        return;
+    }
+
+    tentativas++;
+
+    if (palpite > numeroSecreto) {
+        saida.innerHTML = "O número de entrada é maior";
+    } else if (palpite < numeroSecreto) {
+        saida.innerHTML = "O número de entrada é menor";
+    } else {
+        saida.innerHTML = "Parabéns! Você acertou depois de " + tentativas + " tentativas.";
+        btnEnviarPalpite.disabled = true;
+    }
+
+    // Adicione o palpite à lista de palpites
+    numerosDigitados.push(palpite);
+
+    if (tentativas >= maxTentativas) {
+        saida.innerHTML = "Suas tentativas acabaram. O número secreto era " + numeroSecreto + ".";
+        btnEnviarPalpite.disabled = true;
+    }
+
+    palpites.innerHTML = maxTentativas - tentativas; // Atualize o número de tentativas restantes
+}
